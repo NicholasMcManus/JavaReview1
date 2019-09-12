fcf2cfb078b92162e5ea3e3264755e3fc1d4cf78
NicholasMcManus <mcmanusnj3@gmail.com>
2019-09-12 14:17:59 -0400

    Initialize Files

---
+++ src/fanapplication/FanApplication.java
@@ -0,0 +1,21 @@
+/*
+ * file name: FanApplication.java
+ * programmer name: Nick McManus
+ * date created: 09-11-2019
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+
+package fanapplication;
+
+public class FanApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/fanapplication/fan.java
@@ -0,0 +1,14 @@
+/*
+ * file name: fan.java
+ * programmer name: Nick McManus
+ * date created: 09-12-2019
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+
+package fanapplication;
+
+public class fan {
+
+}
