28ee202966c1bdc13f8aa20ccae4766960b5e0ce
NicholasMcManus <mcmanusnj3@gmail.com>
2019-09-12 14:29:57 -0400

    Create Object Varaibles and Constructors

--- src/fanapplication/fan.java
+++ src/fanapplication/fan.java
@@ -10,5 +10,29 @@
 package fanapplication;
 
 public class fan {
-
+    final public static int SLOW = 1,
+            MEDIUM = 2,
+            FAST = 3;
+    
+    private int fanSpeed;
+    private boolean isOn;
+    private double radius;
+    private String color;
+    
+    public fan()
+    {
+        fanSpeed = this.SLOW;
+        isOn = false;
+        radius = 5;
+        color = "Blue";
+    }
+    
+    public fan(double radius, String color, int fanSpeed, boolean isOn)
+    {
+        this.fanSpeed = fanSpeed;
+        this.isOn = isOn;
+        this.radius = radius;
+        this.color = color;
+    }
+    
 }
